cd2834089d11f4ddb5c7bd5fdf463651000f2d1a
blueburningcoder <f.karg10@gmail.com>
2015-02-08 19:57:46 +0100

    added some more comments
    Failed when trying to add the multidimensional array, returned 139 (0x8B) Segmentation fault -> solved

--- main.cpp
+++ main.cpp
@@ -21,14 +21,25 @@ using namespace sf;
  * drawTile:
  * @param renderWindow: the window the rect is drawn in
  * drawing the rect
+ *
+ * isWall:
+ * @param dir: the direction, 0 = up, 1 = right, 2 = down, 3 = left
  */
 class Tile {
 
     private:
-        int locX, locY, height, width;
+        int locX, locY, height, width, food = 0;
+
+        bool wall[4] = {true, true, true, true};
+//       the directions: up, right, down, left
+
+        Tile* surrounding[4] = {}; // to direct an agent to the next one
+
         RectangleShape rect;
 
     public:
+        // setting the initial @param x: X and @param y: Y values,
+        // as well as the height and width
         void setSize(int x, int y, int height2, int width2){
             locX = x;
             locY = y;
@@ -38,8 +49,32 @@ class Tile {
             rect.setFillColor(Color::Blue);
             rect.setPosition(Vector2f(locX, locY));
         }
+        // Draws the Tile
         void drawTile(RenderWindow *renderWindow){
             renderWindow->draw(rect);
+        }
+        // returns if there is a wall at the @param dir: direction
+        bool isWall(int dir){
+            return wall[dir];
+        }
+        // returns the x value of the Tile
+        int getX(){
+            return locX;
+        }
+        // returns the y value of the Tile
+        int getY(){
+            return locY;
+        }
+        // returns any food if there is some on this Tile, but max 10
+        int getFood(){
+            if(food >= 10){
+                food -= 10;
+                return 10;
+            } else {
+                int tmp = food;
+                food = 0;
+                return tmp;
+            }
         }
 
 };
@@ -49,6 +84,7 @@ class Tile {
  * Maze: including all the Tiles, used to manipulate only specific Tiles
  * @param xSize: how many tiles are gonna be in x direction
  * @param ySize: how many tiles are gonna be in y direction
+ * creating a Maze with with xSize x ySize Tiles
  *
  * drawMaze:
  * @param renderWindow: needed for the Tile.drawTile method to draw on
@@ -61,9 +97,11 @@ class Maze {
         vector<vector<Tile> > MAP;
 
     public:
+        // creating the Maze with @param xSize times @param ySize Tiles
         Maze(int xSize, int ySize){
             sizeX = xSize;
             sizeY = ySize;
+            MAP = vector<vector<Tile> >(xSize, vector<Tile>(ySize));
             for(int i = 0; i < xSize; i++){
                 for(int j = 0; j < ySize; j++){
                     Tile tile;
@@ -73,6 +111,7 @@ class Maze {
                 }
             }
         }
+        // drawing the Maze on the @param renderWindow
         void drawMaze(RenderWindow *renderWindow){
             // TODO: draw Tiles!
             for(int i = 0; i < sizeX; i++){
@@ -139,3 +178,4 @@ int main()
 
 
 
+// TODO: End!
