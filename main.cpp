d86240402ae60b6f147670ff44eb66a4722a0527
blueburningcoder <f.karg10@gmail.com>
2015-02-08 18:52:35 +0100

    added some comments for clarification
    works if built with Code::Blocks.
    
    included the Maze and Tile class within the main file, worked now (again)
    
    Failed when trying to add the multidimensional array, returned 139 (0x8B) Segmentation fault

--- main.cpp
+++ main.cpp
@@ -6,6 +6,22 @@ using namespace std;
 using namespace sf;
 
 
+/*
+ * Tile: the background is out of tiles
+ *
+ * setSize:
+ * @param x: X-location of the Tile later on
+ * @param y: Y-location of the Tile later on
+ * @param height2: the height of the tile
+ * @param width2: the width of the tile
+ * (the 2 is there since the scope resolution operator didn't work for some reason)
+ * setting the size, position and color of the rect
+ *
+ *
+ * drawTile:
+ * @param renderWindow: the window the rect is drawn in
+ * drawing the rect
+ */
 class Tile {
 
     private:
@@ -20,7 +36,7 @@ class Tile {
             width = width2;
             rect.setSize(Vector2f(height, width));
             rect.setFillColor(Color::Blue);
-            rect.setPosition(Vector2f(x, y));
+            rect.setPosition(Vector2f(locX, locY));
         }
         void drawTile(RenderWindow *renderWindow){
             renderWindow->draw(rect);
@@ -29,11 +45,19 @@ class Tile {
 };
 
 
+/*
+ * Maze: including all the Tiles, used to manipulate only specific Tiles
+ * @param xSize: how many tiles are gonna be in x direction
+ * @param ySize: how many tiles are gonna be in y direction
+ *
+ * drawMaze:
+ * @param renderWindow: needed for the Tile.drawTile method to draw on
+ * drawing all Tiles in the MAP
+ */
 class Maze {
 
     private:
         int sizeX = -1, sizeY = -1;
-        Tile tile1;
         vector<vector<Tile> > MAP;
 
     public:
@@ -44,6 +68,7 @@ class Maze {
                 for(int j = 0; j < ySize; j++){
                     Tile tile;
                     tile.setSize(i * 35, j * 35, 30, 30);
+                    // tile might not have been initialized?
                     MAP[i][j] = tile;
                 }
             }
@@ -63,20 +88,21 @@ class Maze {
 int main()
 {
     // Create the main window
-    sf::RenderWindow app(sf::VideoMode(800, 600), "SFML window");
+    RenderWindow app(VideoMode(800, 600), "SFML window");
 
     // Load a sprite to display
-    sf::Texture texture;
-    std::cout << "there's no error yet..." << std::endl;
+    Texture texture;
+    cout << "there's no error yet..." << endl;
     if (!texture.loadFromFile("cb.bmp")) {
-        std::cout << "failed to load!" << std::endl;
+        cout << "failed to load!" << endl;
         return EXIT_FAILURE;
     }
-    sf::Sprite sprite(texture);
+    Sprite sprite(texture);
 
+    cout << "creating the maze ..." << endl;
+
+    // Creating a 10 x 10 Maze
     Maze maze(10, 10);
-
-    int Frame = 0;
 
 	// Start the game loop
     while (app.isOpen())
@@ -90,17 +116,19 @@ int main()
                 app.close();
         }
 
+        cout << "gonna draw it ..." << endl;
+
         // Clear screen
         app.clear();
 
         // Draw the sprite
         app.draw(sprite);
+
+        // drawing the Maze
         maze.drawMaze(&app);
 
         // Update the window
         app.display();
-        std::cout << "FRAME: " << Frame << std::endl;
-        Frame++;
     }
 
     return EXIT_SUCCESS;
