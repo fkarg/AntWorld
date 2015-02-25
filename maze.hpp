2abeea21da21f9d7ef2744ef0dbe2b56788b2c7e
bluebunringcoder <f.karg10@gmail.com>
2015-02-25 19:59:58 +0100

    added some getters

--- maze.hpp
+++ maze.hpp
@@ -32,15 +32,17 @@ class Tile {
 private:
     int locX, locY, height, width, food = 0;
 
+    float foodProduction = 0.0;
+
     bool wall[4] = {true, true, true, true};
 //       the directions: up, right, down, left
 
-    Tile* surrounding[4] = {}; // to direct an agent to the next one
+    Tile *surrounding[4] = {}; // to direct an agent to the next one
 
     RectangleShape rect;
     RectangleShape Walls[4];
 
-    void addWalls(){
+    void addWalls() {
 
         RectangleShape temp;
         temp.setFillColor(Color::Magenta);
@@ -68,9 +70,13 @@ private:
     }
 
 public:
+    Tile(){
+        // ctr
+    }
+
     // setting the initial @param x: X and @param y: Y values,
     // as well as the height and width
-    void setSize(int x, int y, int height2, int width2){
+    void setSize(int x, int y, int height2, int width2) {
         locX = x;
         locY = y;
         height = height2;
@@ -82,20 +88,23 @@ public:
         rect.setFillColor(Color::Blue);
         rect.setPosition(Vector2f(locX, locY));
     }
+
     // Draws the Tile
-    void drawTile(RenderWindow *renderWindow){
+    void drawTile(RenderWindow *renderWindow) {
         renderWindow->draw(rect);
     }
+
     // Draws the Walls of the Tile
-    void drawWalls(RenderWindow *renderWindow){
-        for(int x = 0; x < 4; x++){
+    void drawWalls(RenderWindow *renderWindow) {
+        for (int x = 0; x < 4; x++) {
             if (wall[x]) {
                 renderWindow->draw(Walls[x]);
             }
         }
     }
+
     // moving the rect for @param x: X and @param y: Y pixels ...
-    void move(int x, int y){
+    void move(int x, int y) {
         locX += x;
         locY += y;
 
@@ -103,21 +112,25 @@ public:
 
         rect.setPosition(Vector2f(locX, locY));
     }
+
     // returns if there is a wall at the @param dir: direction
-    bool isWall(int dir){
+    bool isWall(int dir) {
         return wall[dir];
     }
+
     // returns the x value of the Tile
-    int getX(){
+    int getX() {
         return locX;
     }
+
     // returns the y value of the Tile
-    int getY(){
+    int getY() {
         return locY;
     }
+
     // returns any food if there is some on this Tile, but max 10
-    int getFood(){
-        if(food >= 10){
+    int getFood() {
+        if (food >= 10) {
             food -= 10;
             return 10;
         } else {
@@ -127,11 +140,31 @@ public:
         }
     }
 
+    // returns the current 'food' prduction
+    float getFoodProduction() const {
+        return foodProduction;
+    }
+
+// returns all the food currently on the tile
+    int isFood() {
+        return food;
+    }
+
+    // returns true or false respectively to
+    // the mouse inside the tile
+    bool isInside(int x, int y) {
+        return locX <= x && locY <= y
+                && locX + width >= x
+                && locY + height >= y;
+    }
+
+    // drawing the wall and the tile itself combined
+    void draw (RenderWindow *window) {
+        drawTile(window);
+        drawWalls(window);
+    }
+
 };
-
-
-
-
 
 
 /*
@@ -152,14 +185,14 @@ private:
 
 public:
     // creating the Maze with @param xSize times @param ySize Tiles
-    Maze(int xSize, int ySize){
+    Maze(int xSize, int ySize) {
         sizeX = xSize;
         sizeY = ySize;
-        MAP = vector< vector<Tile> >(xSize, vector<Tile>(ySize));
-        for(int i = 0; i < xSize; i++){
-            for(int j = 0; j < ySize; j++){
+        MAP = vector<vector<Tile> >(xSize, vector<Tile>(ySize));
+        for (int i = 0; i < xSize; i++) {
+            for (int j = 0; j < ySize; j++) {
                 Tile tile;
-                tile.setSize(i * 35, j * 35, 30, 30);
+                tile.setSize(i * 31, j * 31, 30, 30);
                 // tile might not have been initialized?
                 MAP[i][j] = tile;
             }
@@ -167,23 +200,33 @@ public:
     }
 
     // drawing the Maze on the @param renderWindow
-    void drawMaze(RenderWindow *renderWindow){
+    void drawMaze(RenderWindow *renderWindow) {
         // TODO: draw Tiles!
-        for(int i = 0; i < sizeX; i++){
-            for(int j = 0; j < sizeY; j++){
-                MAP[i][j].drawTile(renderWindow);
-                MAP[i][j].drawWalls(renderWindow);
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = 0; j < sizeY; j++) {
+                MAP[i][j].draw(renderWindow);
             }
         }
     }
 
     // moving the whole maze for @param x: X and @param y: Y pixels
-    void move(int x, int y){
-        for(int i = 0; i < sizeX; i++){
-            for(int j = 0; j < sizeY; j++){
+    void move(int x, int y) {
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = 0; j < sizeY; j++) {
                 MAP[i][j].move(x, y);
             }
         }
+    }
+
+    // returns the tile the mouse is currently in if it is in one
+    Tile *getTileClicked(int x, int y) {
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = 0; j < sizeY; j++) {
+                if (MAP[i][j].isInside(x, y))
+                    return &MAP[i][j];
+            }
+        }
+        return NULL;
     }
 };
 
