01a92e393e9d588b8ad71c481a2e4f299e94b04e
bluebunringcoder <f.karg10@gmail.com>
2015-02-25 19:59:20 +0100

    Adding GUI elements

--- GraphicsControl.hpp
+++ GraphicsControl.hpp
@@ -3,23 +3,65 @@
 #define THEME_CONFIG_FILE "resources/Black.conf"
 
 #include <TGUI/TGUI.hpp>
+#include "maze.hpp"
 
 
 
 
 class GraphicsControl {
 private:
-    // ...
+    tgui::Label::Ptr InfoLabel;
+    Tile *tile;
+    sf::RenderWindow *window;
 protected:
     // ...
 public:
-    GraphicsControl(){
-        // ctr
+    // setting the window needed later
+    GraphicsControl(sf::RenderWindow *renderWindow){
+        window = renderWindow;
     }
-    // ...
-    void draw(){
-        // ...
+
+    // adding the gui to the window
+    void addGui (tgui::Gui *gui) {
+        tgui::Button::Ptr button( (*gui) );
+        button->load(THEME_CONFIG_FILE);
+        button->setPosition(20, 25);
+        button->setText("Button1");
+        button->setCallbackId(1);
+        button->bindCallback(tgui::Button::LeftMouseClicked);
+        button->setSize(60, 20);
+
+        tgui::Label::Ptr label( (*gui ) );
+        label->load(THEME_CONFIG_FILE);
+        label->setText("Info: \n\n Food: \n\n Scent1: \n\n Scent2: "
+            "\n\n Production: \n\n");
+        label->setPosition(20, 120);
+        label->setTextColor(sf::Color(200, 200, 200));
+        label->setTextSize(12);
+
+        InfoLabel = label;
+
+        // TODO: buttons to change the walls of the currently selected Tile
+
     }
+
+    // changing the displayed info to another @param tile1: tile
+    void changeTextInfoLabel(Tile *tile1) {
+        tile = tile1;
+        updateInfo();
+    }
+
+    // updating the InfoLabel and the seperately displayed tile
+    void updateInfo () {
+        InfoLabel->setText("Info: \n "
+                "\nX: " + std::to_string (tile->getX() ) +
+                "\nY: " + std::to_string (tile->getY() ) +
+                "\n\nFood: \n " +
+                std::to_string (tile->isFood() ) );
+        tile->draw(window);
+    }
+
+
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,145 +1,19 @@
-/// #include <SFML/Graphics.hpp>
-/// #include <iostream>
-/// //// #include "maze.hpp"
-/// //// #include "GraphicsControl.hpp"
-/// #include <TGUI/TGUI.hpp>
-/// 
-/// #define THEME_CONFIG_FILE "resources/Black.conf"
-/// 
-/// 
-/// using namespace std;
-/// using namespace sf;
-/// 
-/// 
-/// 
-/// int main()
-/// {
-///     // Create the main window
-///     RenderWindow window(VideoMode(800, 600), "SFML window");
-/// 
-///     //// GraphicsControl graphicsTGUI;
-/// 
-///     tgui::Gui gui;
-/// 
-///     if (!gui.setGlobalFont("resources/DejaVuSans.ttf"))
-///         return 1;
-/// 
-///     tgui::Picture::Ptr picture(gui);
-///     picture->load("resources/Black.png");
-/// 
-/// 
-///     //// graphicsTGUI.create(window);
-/// 
-///     // ...
-/// 
-/// 
-///     cout << "gonna create d buttöööön " << endl;
-/// 
-/// 
-///     tgui::Button::Ptr button(gui);
-///     button->load(THEME_CONFIG_FILE);
-///     button->setPosition(40, 25);
-///     button->setText("Quit");
-///     button->setCallbackId(1);
-///     button->bindCallback(tgui::Button::LeftMouseClicked);
-///     button->setSize(300, 40);
-/// 
-///     cout << "created button" << endl;
-/// 
-///     // Load a sprite to display
-///     // Texture texture;
-///     // cout << "there's no error yet..." << endl;
-///     // if (!texture.loadFromFile("cb.bmp")) {
-///     //     cout << "failed to load!" << endl;
-///     //     return EXIT_FAILURE;
-///     // }
-///     // Sprite sprite(texture);
-/// 
-///     //// cout << "creating the maze ..." << endl;
-/// 
-///     // Creating a 10 x 10 Maze
-///     //// Maze maze(10, 10);
-/// 
-///     // Frame-counter
-///     int Frame = 0;
-/// 
-/// 	// Start the game loop
-///     while (window.isOpen())
-///     {
-///         // Process events
-///         Event event;
-///         while (window.pollEvent(event))
-///         {
-///             // Close window : exit
-///             if (event.type == sf::Event::Closed)
-///                 window.close();
-/// 
-///             //// graphicsTGUI.handleEvent(event);
-///             gui.handleEvent(event);
-///         }
-/// 
-///         cout << "creating callback" << endl;
-/// 
-///         tgui::Callback callback;
-/// 
-///         while(gui.pollCallback(callback)){
-///             if (callback.id == 1)
-///                 window.close();
-/// 
-///             else if (callback.id == 2)
-///             {
-///                 if (callback.text == "Exit")
-///                     window.close();
-///             }
-///         }
-/// 
-///         //// while(graphicsTGUI.pollCallback(callback)){
-///         ////     if(callback.id == 1)
-///         ////         window.close();
-///         //// }
-/// 
-///         cout << "clearing the window ..." << endl;
-/// 
-///         // Clear screen
-///         window.clear();
-/// 
-///         // drawing the Maze
-///         //// maze.drawMaze(&window);
-/// 
-///         //// graphicsTGUI.draw();
-/// 
-///         cout << "drawing gui ..." << endl;
-/// 
-///         gui.draw();
-/// 
-///         cout << "displaying it ... " << endl;
-/// 
-///         // Update the window
-///         window.display();
-/// 
-///         // shows the Frame number
-///         cout << "Frame: " << Frame << endl;
-///         Frame++;
-///     }
-/// 
-///     return EXIT_SUCCESS;
-/// }
-/// 
-/// 
-/// 
-/// 
-
-
 #include <TGUI/TGUI.hpp>
 #include "GraphicsControl.hpp"
 #include "maze.hpp"
+#include <iostream>
 
 #define THEME_CONFIG_FILE "resources/Black.conf"
 
 int main()
 {
+
+    std::cout << "creating window and gui ..." << std::endl;
+
     sf::RenderWindow window(sf::VideoMode(800, 600), "TGUI window");
     tgui::Gui gui(window);
+
+    std::cout << "setting global font ..." << std::endl;
 
     if (!gui.setGlobalFont("resources/DejaVuSans.ttf"))
         return 1;
@@ -147,19 +21,29 @@ int main()
     /// tgui::Picture::Ptr picture(gui);
     /// picture->load("resources/Black.png");
 
-    tgui::Button::Ptr button(gui);
-    button->load(THEME_CONFIG_FILE);
-    button->setPosition(40, 25);
-    button->setText("Button1");
-    button->setCallbackId(1);
-    button->bindCallback(tgui::Button::LeftMouseClicked);
-    button->setSize(300, 40);
+    std::cout << "creating Button ..." << std::endl;
+
+    // creating the GraphicsControl
+    GraphicsControl control (&window);
+    control.addGui(&gui);
+
+    std::cout << "creating maze ... " << std::endl;
 
     Maze maze(10, 10);
 
-    maze.move(40, 90);
+    Tile tile;
+
+    tile.setSize(40, 60, 35, 35);
+
+    control.changeTextInfoLabel(&tile);
+
+    std::cout << "moving maze ... " << std::endl;
+
+    maze.move(120, 90);
 
     int Frame = 0;
+
+    std::cout << "starting main loop ... " << std::endl;
 
     while (window.isOpen())
     {
@@ -168,6 +52,10 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            // TODO: mouse click!
+            // if (event.type == sf::Mouse::isButtonPressed(sf::Mouse::Left))
+
 
             gui.handleEvent(event);
         }
@@ -185,11 +73,18 @@ int main()
             }
         }
 
+
+
+
+
         window.clear();
 
         maze.drawMaze(&window);
 
+        control.updateInfo();
+
         gui.draw();
+
         window.display();
         std::cout << "Frame: " << Frame << std::endl;
         Frame++;
