09eff29f7fa6bfadefd06e3a1066b7c8d492362c
bluebunringcoder <f.karg10@gmail.com>
2015-03-20 06:52:10 +0100

    added comments

--- maze.cpp
+++ maze.cpp
@@ -1,6 +1,13 @@
 #include "maze.h"
 
 
+///////////////////////////////////////////////////////////////////
+////                    Tile
+///////////////////////////////////////////////////////////////////
+
+
+
+// adding or updating the Walls of the Tile
 void Tile::addWalls() {
     sf::RectangleShape temp;
     temp.setFillColor(sf::Color::Magenta);
@@ -21,6 +28,7 @@ void Tile::addWalls() {
 }
 
 
+// creating the tile at @param x and @param y with @param width and @param height
 void Tile::setSize(int x, int y, int width, int height) {
     locX = x;
     locY = y;
@@ -35,16 +43,19 @@ void Tile::setSize(int x, int y, int width, int height) {
 }
 
 
+// returns the x location of the Tile
 int Tile::getX() {
     return locX;
 }
 
 
+// returns the Y location of the Tile
 int Tile::getY() {
     return locY;
 }
 
 
+// moves the Tile at @param x and @param y pixels
 void Tile::move(int x, int y) {
     locX += x;
     locY += y;
@@ -55,6 +66,7 @@ void Tile::move(int x, int y) {
 }
 
 
+// returns if the @param x and @param y coordinates are inside the tile
 bool Tile::isInside(int x, int y) {
     return locX <= x && locY <= y
             && locX + width >= x
@@ -62,42 +74,50 @@ bool Tile::isInside(int x, int y) {
 }
 
 
+// returns the height of the Tile
 int Tile::getHeight() {
     return height;
 }
 
 
+// returns the width of the Tile
 int Tile::getWidth() {
     return width;
 }
 
 
+// setting the @param index
 void Tile::setIndex(int index) {
     Tile::index = index;
 }
 
 
+// returns the index of the Tile
 int Tile::getIndex() const {
     return index;
 }
 
 
+// setting the color of the Tile to the @param color
 void Tile::setColor(sf::Color color) {
     rect.setFillColor(color);
 }
 
 
+// drawing the Tile completely
 void Tile::draw(sf::RenderWindow *renderWindow) {
     drawTile(renderWindow);
     drawWalls(renderWindow);
 }
 
 
+// only drawing the Tile
 void Tile::drawTile(sf::RenderWindow *renderwindow) {
     renderwindow->draw(rect);
 }
 
 
+// drawing the walls -> dependent on wall[x]
 void Tile::drawWalls(sf::RenderWindow *renderWindow) {
     for (int x = 0; x < 4; x++) {
         if (wall[x])
@@ -106,16 +126,20 @@ void Tile::drawWalls(sf::RenderWindow *renderWindow) {
 }
 
 
+// updating things at a tick
 void Tile::doTick() {
     // for implementation purposes
 }
 
 
+// setting in @param dir the wall to @param setWall
 void Tile::setWall(int dir, bool setWall) {
     if(isSurrounding(dir) )
         wall[dir] = setWall;
 }
 
+
+// returns if there is a Wall in @param dir
 bool Tile::isWall(int dir) {
     if (isSurrounding(dir) )
         return wall[dir];
@@ -123,27 +147,32 @@ bool Tile::isWall(int dir) {
 }
 
 
+// setting in @param dir the surrounding @param tile
 void Tile::setSurrounding(int dir, Tile *tile) {
     surrounding[dir] = tile;
 }
 
 
+// getting the surrounding tile in @param dir
 Tile* Tile::getSurrounding(int dir) {
     return surrounding[dir];
 }
 
 
+// returns if there is a tile surrounding in @param dir
 bool Tile::isSurrounding(int dir) {
     return dir % 4 == dir && surrounding[dir] != NULL;
 }
 
 
-
+// returns if there is food on the tile and how much
 int Tile::isFood() {
     return food;
 }
 
 
+// returns the current food on the tile but max. 10 at once
+// decreases the food on the tile at the same time
 int Tile::getFood() {
     if (food >= 10) {
         food -= 10;
@@ -158,51 +187,62 @@ int Tile::getFood() {
 
 
 
+///////////////////////////////////////////////////////////////////
+////                        showTile
+///////////////////////////////////////////////////////////////////
 
 
 
-
+// overrides the '=' operator for the @param tile
 void showTile::operator=(Tile *tile) {
     tileToShow = tile;
     doTick();
 }
 
 
+// returns the height of the current tileToShow
 int showTile::getHeight() {
     return pubHeight;
 }
 
 
+// returns the width of the current tileToShow
 int showTile::getWidth() {
     return pubWidth;
 }
 
 
+// returns the X-pos of the tileToShow
 int showTile::getX() {
     return pubX;
 }
 
 
+// returns the Y-pos of the tileToShow
 int showTile::getY() {
     return pubY;
 }
 
 
+// setting the @param index of the 'pub' tile
 void showTile::setIndex(int index) {
     showTile::pubIndex = index;
 }
 
 
+// returns the index of the tileToShow
 int showTile::getIndex() {
     return pubIndex;
 }
 
 
+// returns all the food of the tileToShow
 int showTile::isFood() {
     return pubFood;
 }
 
 
+// setting in the @param dir the wall to @param setWall
 void showTile::setWall(int dir, bool setWall) {
     if (dir % 4 == dir) {
         tileToShow->setWall(dir, setWall);
@@ -211,6 +251,7 @@ void showTile::setWall(int dir, bool setWall) {
 }
 
 
+// actualizing whatever happened on the tile
 void showTile::doTick() {
     if (tileToShow != NULL) {
         pubX = tileToShow->getX();
@@ -228,16 +269,19 @@ void showTile::doTick() {
 }
 
 
+// returns if there is a tile virtually surrounding this tile in @param dir
 bool showTile::isSurrounding(int dir) {
     return tileToShow->isSurrounding(dir);
 }
 
 
+// returns the tile in @param dir of the tileToShow
+// (since there is no tile next to the showTile anyways)
 Tile* showTile::getSurrounding(int dir) {
     return tileToShow->getSurrounding(dir);
 }
 
-
+// returns the tile that is being shown
 Tile* showTile::getTileToShow() {
     return tileToShow;
 }
@@ -246,7 +290,13 @@ Tile* showTile::getTileToShow() {
 
 
 
+///////////////////////////////////////////////////////////////////
+////                        Maze
+///////////////////////////////////////////////////////////////////
 
+
+
+// (private) drawing the outer walls of the maze
 void Maze::drawOuterWalls(sf::RenderWindow *renderWindow) {
     for (int x = 0; x < 4; x++) {
         renderWindow->draw((OuterWalls[x] ) );
@@ -254,6 +304,7 @@ void Maze::drawOuterWalls(sf::RenderWindow *renderWindow) {
 }
 
 
+// initiating a maze with @param xSize times @param ySize Tiles
 Maze::Maze(int xSize, int ySize) {
     sizeX = xSize;
     sizeY = ySize;
@@ -272,6 +323,7 @@ Maze::Maze(int xSize, int ySize) {
 }
 
 
+// setting or actualizing the outerWalls of the Maze
 void Maze::setOuterWalls() {
 
     sf::RectangleShape temp;
@@ -300,6 +352,7 @@ void Maze::setOuterWalls() {
 }
 
 
+// setting the neighbourTile of each tile in the maze
 void Maze::setNeighbourTiles() {
     for (int i = 0; i < sizeX; i++) {
         for (int j = 0; j < sizeY; j++) {
@@ -316,6 +369,7 @@ void Maze::setNeighbourTiles() {
 }
 
 
+// drawing the whole maze (first tiles, then walls, then OuterWalls)
 void Maze::drawMaze(sf::RenderWindow *renderWindow) {
     for (int i = 0; i < sizeX; i++) {
         for (int j = 0; j < sizeY; j++) {
@@ -331,6 +385,7 @@ void Maze::drawMaze(sf::RenderWindow *renderWindow) {
 }
 
 
+//moving the whole maze for @param x and @param y pixels
 void Maze::move(int x, int y) {
     Maze::x += x;
     Maze::y += y;
@@ -343,6 +398,7 @@ void Maze::move(int x, int y) {
 }
 
 
+// doing the tick for every tile
 void Maze::doTick() {
     for (int i = 0; i < sizeX; i++) {
         for (int j = 0; j < sizeY; j++) {
@@ -352,6 +408,7 @@ void Maze::doTick() {
 }
 
 
+// returns the tile that got clicked on (if there is one)
 Tile* Maze::getTileClicked(int x, int y) {
     for (int i = 0; i < sizeX; i++) {
         for (int j = 0; j < sizeY; j++) {
@@ -363,26 +420,31 @@ Tile* Maze::getTileClicked(int x, int y) {
 }
 
 
+// returns the tile at the @param index
 Tile* Maze::getTile(int index) {
     return &MAP[index / sizeX][index % sizeY];
 }
 
 
+// returning the tile at the @param x and @param y coordinates
 Tile* Maze::getTile(int x, int y) {
     return &MAP[x][y];
 }
 
 
+// returning the sizeX of the maze
 int Maze::getSizeX() {
     return sizeX;
 }
 
 
+// returning the the sizeY of the the maze
 int Maze::getSizeY() {
     return sizeY;
 }
 
 
+// returning the max index of the maze
 int Maze::INDEX_MAX() {
     return sizeX * sizeY;
 }
