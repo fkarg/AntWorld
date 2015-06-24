960c77582030dfd74115fbd57aab03c2667debed
blueburningcoder <f.karg10@gmail.com>
2015-06-24 06:49:27 +0200

    fixed sensing, more debuginfo, TODO: rewrite decide

--- ai.cpp
+++ ai.cpp
@@ -2,8 +2,22 @@
 #include "ant.h"
 
 
+
+void Surrounding_state::print() const {
+    std::cout << "Surrounding - INFO" << std::endl;
+    std::cout << "Walls and Scent: " << std::endl;
+    for (int dir = 0; dir < 4; dir++)
+        std::cout << "Dir: " << dir << " wall: " << walls[dir]
+				<< " scent: " << scents[4] << std::endl;
+    std::cout << "isFood: " << isFood << " isBase: " << isBase
+			<< " foodThere: " << foodThere << " antFood: " << antFood
+			<< std::endl;
+}
+
+
 // sensing, reacting and moving
 void ai::senseAndAct(Ant* itself) {
+	std::cout << "Ant " << itself->getID() << " is doing sth" << std::endl;
 	DO(decide(sense(itself) ), itself);
 }
 
@@ -13,54 +27,76 @@ Surrounding_state ai::sense(Ant* ant) {
 	Surrounding_state current;
 	for (int dir = 0; dir < 4; dir++ ) {
 		current.walls[dir] = ant->getCurrent()->isWall(dir);
-		current.scents[dir] = ant->getCurrent()->getTeamScent(ant);
+		if (!current.walls[dir])
+			current.scents[dir] = ant->getCurrent()->getTeamScent(ant);
 	}
 	current.isBase = ant->getCurrent()->isBASE();
 	current.isFood = ant->getCurrent()->isFood() > 0;
 	if (current.isFood)
-		current.food_there = ant->getCurrent()->isFood();
-    current.ant_food = ant->getFood();
+		current.foodThere = ant->getCurrent()->isFood();
+    current.antFood = ant->getFood();
+    current.lastAction = ant->getLastAction();
+
+	if (ant->getFood() > 15 || ant->getFood() <= 5)
+		current.searchingHome = true;
+	else if (ant->getFood() < 15)
+		current.searchingFood = true;
 
 	return current;
 }
 
+
+/*
 // @returns the decision based on the @param state of the surroundings
 ACTION ai::decide(Surrounding_state currentState) {
+
+    currentState.print();
+
 	ACTION decision = STAY;
 
 	int highest = 0, dirTo = -1, possDirs = 0;
 
-	for (int dir = 0; dir < 4; dir++) {
+	for (int dir = 0; dir < 4 && (dir + 2) % 4 != currentState.lastAction; dir++) {
 		if (currentState.scents[dir] > highest)
 			highest = currentState.scents[dir], dirTo = dir;
 		if (!currentState.walls[dir] )
 			possDirs++;
-	}
+	}bg
 
 	if (possDirs >= 1)
 		while (decision == STAY) {
 			int dir = rand() % 4;
-			if (!currentState.walls[dir])
+			if (!currentState.walls[dir] && (dir + 2) % 4 != currentState.lastAction)
 				decision = (ACTION) dir;
 		}
-	else if (dirTo >= 0) decision = (ACTION) dirTo;
+	else if (dirTo >= 0 && (dirTo + 2 % 4) != currentState.lastAction) decision = (ACTION) dirTo;
 
-	if (currentState.isFood && currentState.food_there > 0)
+	if (currentState.isFood && currentState.foodThere > 0
+            && currentState.antFood < MAX_FOOD_ANT_CARRYING - 2)
 		decision = TAKE_FOOD;
 
 	if (currentState.isBase)
-		if (currentState.ant_food < 10)
+		if (currentState.antFood < 10)
 			decision = TAKE_FOOD;
-		else if (currentState.ant_food > 10)
+		else if (currentState.antFood > 10)
 			decision = GIVE_FOOD;
 
 	return decision;
+}
+*/
+
+
+ACTION ai::decide(const Surrounding_state &currentState) {
+	currentState.print();
+
+	return STAY
 }
 
 
 // actually doing @param what with the @param ant
 void ai::DO(ACTION what, Ant* ant) {
 	// TODO: do what the action says
+    std::cout << "doing: " << what << " for ant: " << ant->getID() << std::endl;
 	switch (what) {
         case MOVE_UP:
         case MOVE_RIGHT:
@@ -73,8 +109,10 @@ void ai::DO(ACTION what, Ant* ant) {
             break;
         case GIVE_FOOD:
             // ant->getCurrent()->getBase()->addFood(ant->getFood())
+            // TODO: method ant-sided
             break;
         case STAY:
             break;
     }
+	ant->setLastAction(what);
 }
--- ai.h
+++ ai.h
@@ -9,17 +9,19 @@ class Ant;
 
 
 enum ACTION {
-    MOVE_UP, MOVE_RIGHT, MOVE_DOWN, MOVE_LEFT,
-    TAKE_FOOD, GIVE_FOOD, STAY
+	MOVE_UP, MOVE_RIGHT, MOVE_DOWN, MOVE_LEFT,
+	TAKE_FOOD, GIVE_FOOD, STAY
 };
 
 
 
 struct Surrounding_state {
-	bool walls[4] = { false };
-	int scents[4] = { -1, -1, -1, -1 };
-	bool isBase = false, isFood = false;
-	int food_there = 0, ant_food;
+	bool walls[4] = { false }; // if there's a wall in dir
+	int scents[4] = { -1, -1, -1, -1 }; // how much scent there's in dir
+	bool isBase = false, isFood = false; // if there's a BASE or RES on this tile
+	bool searchingHome = false, searchingFood = false; // if searching for food or for the base currently
+	int foodThere = 0, antFood = 0, lastAction = 6;
+	void print() const;       // printing debug-info
 };
 
 
@@ -28,12 +30,13 @@ private:
     float version = 1.002;
 
 public:
-	ai(){};     // FIXME: currently crashing
+	ai(){};     // FIXME: currently crashing for usual ants
 	void senseAndAct(Ant* itself);  // sensing, reacting and moving altogether
 	Surrounding_state sense(Ant* ant);  // @returns the surrounding state around the @param ant
-	ACTION decide(Surrounding_state currentState);  // @returns decision what to do based on the @param surrounding conditions
+	ACTION decide(const Surrounding_state& currentState);  // @returns decision what to do based on the @param surrounding conditions
 	void DO(ACTION what, Ant* ant);  // actually doing @param what with the @param ant
 };
 
 
 #endif //ANTWORLD_AI_H
+
